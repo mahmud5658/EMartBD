b3fd8028abd202233ae3a51970c0c3769c33186a
Abdullah <almahmuda5@gmail.com>
2023-08-11 02:11:57 +0600

    E-Mart BD

--- app/src/main/java/org/abdsoft/emartbd/RegisterSellerActivity.java
+++ app/src/main/java/org/abdsoft/emartbd/RegisterSellerActivity.java
@@ -3,12 +3,73 @@ package org.abdsoft.emartbd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class RegisterSellerActivity extends AppCompatActivity {
+
+    private ImageButton backBtn, gpsBtn;
+    private ImageView profileIv;
+    private EditText nameEt, shopNameEt, phoneEt, deliveryFeeEt, countryEt, stateEt, cityEt,
+            addressEt, emailEt, passwordEt, cpasswordEt;
+    private Button registerBtn;
+
+    // permission constant
+    private static final int LOCATION_CODE=100;
+    // permission arrays
+    private String[] locationPermissions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_seller);
+
+        backBtn = findViewById(R.id.backBtn);
+        gpsBtn = findViewById(R.id.gpsBtn);
+        profileIv = findViewById(R.id.profileIv);
+        nameEt = findViewById(R.id.nameEt);
+        shopNameEt = findViewById(R.id.shopNameEt);
+        phoneEt = findViewById(R.id.phoneEt);
+        deliveryFeeEt = findViewById(R.id.deliveryFeeEt);
+        countryEt = findViewById(R.id.countryEt);
+        stateEt = findViewById(R.id.stateEt);
+        cityEt = findViewById(R.id.cityEt);
+        addressEt = findViewById(R.id.addressEt);
+        emailEt = findViewById(R.id.emailEt);
+        passwordEt = findViewById(R.id.passwordEt);
+        cpasswordEt = findViewById(R.id.cpasswordEt);
+        registerBtn = findViewById(R.id.registerBtn);
+        
+        // init permission array
+//        locationPermissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        gpsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //detect current location
+            }
+        });
+        profileIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //pick image from gallery
+            }
+        });
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // register user
+            }
+        });
     }
 }
