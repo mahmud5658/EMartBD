67220f3534e0e3ad5ad5858c7d55c82fd01c6edd
Abdullah <almahmuda5@gmail.com>
2023-08-25 16:12:54 +0600

    E-Mart BD

--- app/src/main/java/org/abdsoft/emartbd/ProfileEditUserActivity.java
+++ app/src/main/java/org/abdsoft/emartbd/ProfileEditUserActivity.java
@@ -1,19 +1,36 @@
 package org.abdsoft.emartbd;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class ProfileEditUserActivity extends AppCompatActivity {
+import java.util.List;
+import java.util.Locale;
+
+public class ProfileEditUserActivity extends AppCompatActivity implements LocationListener {
 
     private ImageButton backBtn, gpsBtn;
     private ImageView profileIv;
@@ -26,15 +43,22 @@ public class ProfileEditUserActivity extends AppCompatActivity {
     // image pick constant
     private static final int IMAGE_PICK_GALLERY_CODE = 400;
     private static final int IMAGE_PICK_CAMERA_CODE = 500;
-    // progress dialog
-    private ProgressDialog progressDialog;
-    // firebase auth
-    private FirebaseAuth firebaseAuth;
 
     // permission arrays
     private String[] locationPermissions;
     private String[] cameraPermission;
     private String[] storagePermission;
+
+    // progress dialog
+    private ProgressDialog progressDialog;
+
+    // firebase auth
+    private FirebaseAuth firebaseAuth;
+
+    private LocationManager locationManager;
+    private double latitude = 0.0;
+    private double longitude = 0.0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +100,12 @@ public class ProfileEditUserActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // detect location
+                if (checkLocationPermission()) {
+                    // already allow
+                    detectLocation();
+                } else {
+                    requestLocationPermissions();
+                }
             }
         });
 
@@ -85,5 +115,89 @@ public class ProfileEditUserActivity extends AppCompatActivity {
                 //begin update profile
             }
         });
+    }
+
+    private void requestLocationPermissions() {
+        ActivityCompat.requestPermissions(this, locationPermissions, LOCATION_REQUEST_CODE);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void detectLocation() {
+        Toast.makeText(this, "Please wait", Toast.LENGTH_SHORT).show();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
+
+    private boolean checkLocationPermission() {
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    public void findAddress() {
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            String address = addresses.get(0).getAddressLine(0);
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+
+            // set address
+            countryEt.setText(country);
+            cityEt.setText(city);
+            stateEt.setText(state);
+            addressEt.setText(address);
+
+        } catch (Exception e) {
+            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        findAddress();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LocationListener.super.onStatusChanged(provider, status, extras);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        Toast.makeText(this, "Location is disabled...", Toast.LENGTH_SHORT).show();
+        LocationListener.super.onProviderDisabled(provider);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    boolean locationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (locationAccepted) {
+                        //  permission allow
+                        detectLocation();
+                    } else {
+                        Toast.makeText(this, "Location Permission is necessary...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
